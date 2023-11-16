bf0d75abab5508da030b46dbc75e2481ce839059
RudraUX <pollabrock@gmail.com>
2023-11-16 18:15:26 +0600

    clerk added

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Admin</h1>
+      <Button>Click Me</Button>
+    </main>
+  );
+}
