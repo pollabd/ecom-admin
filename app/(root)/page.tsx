63525b17dcf7dcaa666343a92e7ed35529054ce6
RudraUX <pollabrock@gmail.com>
2023-11-20 18:34:21 +0600

    dilog bug fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import Modal from "@/components/ui/modal";
+import useStoreModal from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 export default function SetupPage() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
     <main>
-      <div className="p-4">
-        <Modal title="'test" description="test des" isOpen onClose={() => {}}>
-          chat
-        </Modal>
-      </div>
+      <div className="p-4">root page</div>
     </main>
   );
 }
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import React from "react";
-import Modal from "../ui/modal";
+import Modal from "@/components/ui/modal";
+import useStoreModal from "@/hooks/use-store-modal";
 
 export const StoreModal = () => {
+  const storeModal = useStoreModal();
   return (
     <Modal
       title="Create Store"
-      description="Add a new store"
-      isOpen={false}
-      onClose={() => {}}
+      description="Add a new store to manage products and categories"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
     >
       Future modal
     </Modal>
